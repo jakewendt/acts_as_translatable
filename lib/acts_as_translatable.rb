f5e35c1d83b3e1c33fe471ea7aaeffb74104df77
Jake <github@jake.otherinbox.com>
2010-05-03 19:36:48 -0700

    Added inits and some structure

--- lib/acts_as_translatable.rb
+++ lib/acts_as_translatable.rb
@@ -1,1 +1,24 @@
 # ActsAsTranslatable
+module Acts #:nodoc:
+	module Translatable #:nodoc:
+		def self.included(base)
+			base.extend(InitialClassMethods)
+		end
+
+		module InitialClassMethods
+			def acts_as_translatable(options = {})
+				configuration = { }
+				configuration.update(options) if options.is_a?(Hash)
+
+				include Acts::Translatable::InstanceMethods
+				extend  Acts::Translatable::ClassMethods
+			end
+		end
+
+		module ClassMethods
+		end 
+
+		module InstanceMethods
+		end 
+	end
+end
