55d3b364c59563410b0803387b35da6e8eb16a6a
Jake <github@jake.otherinbox.com>
2010-05-04 09:17:05 -0700

    Finished setup

--- generators/acts_as_translatable/acts_as_translatable_generator.rb
+++ generators/acts_as_translatable/acts_as_translatable_generator.rb
@@ -3,6 +3,8 @@ class ActsAsTranslatableGenerator < Rails::Generator::NamedBase
     record do |m|
       # m.directory "lib"
       # m.template 'README', "README"
+			m.migration_template 'migration.rb', 'db/migrate', 
+				:migration_file_name => "add_translatability_to_#{file_path.gsub(/\//, '_').pluralize}"
     end
   end
 end
---
+++ test/001_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration
+	def self.up
+		create_table :pages do |t|
+			t.string :title
+			t.text :body
+		end
+	end
+
+	def self.down
+		drop_table :pages
+	end
+end
