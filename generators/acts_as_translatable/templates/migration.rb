4cefafc6523b6dd50deb2e6440b23e012385c4d9
Jake <github@jake.otherinbox.com>
2010-05-05 11:29:07 -0700

    Really added remove_index to migration; and added loop to set translatable_id to id; (don't like doing that though)

--- generators/acts_as_translatable/templates/migration.rb
+++ generators/acts_as_translatable/templates/migration.rb
@@ -6,10 +6,13 @@ class AddTranslatabilityTo<%= class_name.pluralize.gsub(/::/, '') -%> < ActiveRe
 			:locale, :string, :default => 'en', :null => false
 		add_index  :<%= file_path.gsub(/\//, '_').pluralize -%>, 
 			[:translatable_id, :locale], :unique => true
+		<%=class_name-%>.all.each do |p|
+			p.update_attribute(:translatable_id, p.id)
+		end
 	end
 
 	def self.down
-		add_index  :<%= file_path.gsub(/\//, '_').pluralize -%>, 
+		remove_index  :<%= file_path.gsub(/\//, '_').pluralize -%>, 
 			[:translatable_id, :locale]
 		remove_column :<%= file_path.gsub(/\//, '_').pluralize -%>, 
 			:translatable_id
